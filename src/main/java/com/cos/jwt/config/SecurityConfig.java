fd68dfd7c646e995d0f297fa8fcefdc67b971ec3
weekbelt <vfrvfr4207@gmail.com>
2022-03-31 23:30:59 +0900

    스프링부트 시큐리티 22강 - jwt FIlter 등록 테스트

--- src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
@@ -18,9 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-        ;
-
+            .addFilterBefore(new MyFilter3(), SecurityContextPersistenceFilter.class);
+        http
+            .csrf().disable();
         http
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)    // 세션 사용하지 않겠다.
