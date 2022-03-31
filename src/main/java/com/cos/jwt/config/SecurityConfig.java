97c7d697a9f128257f5be493a50b6bbd2bddbd58
weekbelt <vfrvfr4207@gmail.com>
2022-03-31 23:02:46 +0900

    jwt를 위한 security 설정

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.cos.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+        ;
+
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)    // 세션 사용하지 않겠다.
+            .and()
+            .addFilter(corsFilter)      // @CrossOrigin(인증X), 시큐리티 필터에 등록 인증(O)
+            .formLogin().disable()
+            .httpBasic().disable()
+            .authorizeRequests()
+            .antMatchers("/api/v1/user/**")
+            .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+            .antMatchers("/api/v1/manager/**")
+            .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+            .antMatchers("/api/v1/admin/**")
+            .access("hasRole('ROLE_ADMIN')")
+            .anyRequest().permitAll()
+        ;
+    }
+}
---
+++ src/main/java/com/cos/jwt/model/User.java
@@ -0,0 +1,42 @@
+package com.cos.jwt.model;
+
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    private String password;
+
+    private String roles;   // USER, ADMIN
+
+    @Builder
+    public User(String username, String password, String roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+            return List.of(this.roles.split(","));
+        } else {
+            return Collections.emptyList();
+        }
+    }
+}
