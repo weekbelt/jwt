1409165fed509a9a0ae6b1836ab67d1a1e874a14
weekbelt <vfrvfr4207@gmail.com>
2022-03-31 23:49:57 +0900

    스프링부트 시큐리티 23강 - jwt 임시 토큰 만들어서 테스트 해보기

--- src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -11,6 +11,7 @@ public class MyFilter1 implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
         System.out.println("필터 1");
         chain.doFilter(request, response);
     }
